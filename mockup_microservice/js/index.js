001f6ee7a8baa00d433d0d82cf6bf3eec8454323
MatiM72737 <72034316+MatiM72737@users.noreply.github.com>
2022-10-20 13:53:18 +0200

    Add files via upload

--- mockup_microservice/js/index.js
+++ mockup_microservice/js/index.js
@@ -7,15 +7,39 @@ var table = document.querySelector("table");
 
 textInput.addEventListener("input", updateTable);
 
+var firstTr = document.createElement("tr");
+var fTd1 = document.createElement("td");
+var fTd2 = document.createElement("td");
+var fTd3 = document.createElement("td");
+var fTd4 = document.createElement("td");
+fTd1.innerHTML = "Name";
+fTd2.innerHTML = "SureName";
+fTd3.innerHTML = "Age";
+fTd4.innerHTML = "Country";
+firstTr.appendChild(fTd1);
+firstTr.appendChild(fTd2);
+firstTr.appendChild(fTd3);
+firstTr.appendChild(fTd4);
+
 function updateTable(event) {
   var text = event.target.value;
   var tab = text.split(";");
   table.textContent = "";
+  table.appendChild(firstTr);
   tab.forEach((rowtext) => {
     var tr = document.createElement("tr");
-    var td = document.createElement("td");
-    td.innerHTML = rowtext;
-    tr.appendChild(td);
+    var tdName = document.createElement("td");
+    var tdSureName = document.createElement("td");
+    var tdAge = document.createElement("td");
+    var tdCountry = document.createElement("td");
+    tdName.innerHTML = rowtext.split(",")[0];
+    tdSureName.innerHTML = rowtext.split(",")[1];
+    tdAge.innerHTML = rowtext.split(",")[2];
+    tdCountry.innerHTML = rowtext.split(",")[3];
+    tr.appendChild(tdName);
+    tr.appendChild(tdSureName);
+    tr.appendChild(tdAge);
+    tr.appendChild(tdCountry);
     table.appendChild(tr);
   });
 }
